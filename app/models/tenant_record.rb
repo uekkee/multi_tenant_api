3391f95eac6afa6bf656dfa071daeb8a2d6fdf52
uekkee <uekkee@gmail.com>
2021-10-06 13:09:22 +0900

    change connections of shards by HashConfig

--- app/models/tenant_record.rb
+++ app/models/tenant_record.rb
@@ -2,7 +2,47 @@ class TenantRecord < ApplicationRecord
   self.abstract_class = true
 
   connects_to shards: {
-    shard_tenant_1: { writing: :tenant_1, reading: :tenant_1 },
-    shard_tenant_2: { writing: :tenant_2, reading: :tenant_2 },
+    shard_tenant_1: {
+      writing:
+        {
+          "adapter"=>"postgresql",
+          "encoding"=>"unicode",
+          "pool"=>5,
+          "host"=>"127.0.0.1",
+          "username"=>"postgres",
+          "password"=>nil,
+          "database"=>"multi_tenant_api_tenant_1_development"
+        },
+      reading:
+        {"adapter"=>"postgresql",
+         "encoding"=>"unicode",
+         "pool"=>5,
+         "host"=>"127.0.0.1",
+         "username"=>"postgres",
+         "password"=>nil,
+         "database"=>"multi_tenant_api_tenant_1_development"
+        },
+    },
+    shard_tenant_2: {
+      writing:
+        {
+          "adapter"=>"postgresql",
+          "encoding"=>"unicode",
+          "pool"=>5,
+          "host"=>"127.0.0.1",
+          "username"=>"postgres",
+          "password"=>nil,
+          "database"=>"multi_tenant_api_tenant_2_development"
+        },
+      reading:
+        {"adapter"=>"postgresql",
+         "encoding"=>"unicode",
+         "pool"=>5,
+         "host"=>"127.0.0.1",
+         "username"=>"postgres",
+         "password"=>nil,
+         "database"=>"multi_tenant_api_tenant_2_development"
+        },
+    },
   }
 end
