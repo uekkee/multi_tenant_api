303c2791d9224295c8afed1bf3394693558a3c0f
uekkee <uekkee@gmail.com>
2021-10-06 12:26:57 +0900

    enable switch tenant by DB shard

--- app/controllers/tenanted_controller.rb
+++ app/controllers/tenanted_controller.rb
@@ -1,2 +1,16 @@
 class TenantedController < ApplicationController
+  around_action :detect_tenant_db
+
+  private
+
+  def detect_tenant_db
+    TenantRecord.connected_to(shard: shard_name, role: :writing) do
+      yield
+    end
+  end
+
+  def shard_name
+    tenant_id = request.path.match(/\/tenants\/(\d+)\//)[1]
+    :"shard_tenant_#{tenant_id}"
+  end
 end
--- app/models/tenant_record.rb
+++ app/models/tenant_record.rb
@@ -1,5 +1,8 @@
 class TenantRecord < ApplicationRecord
   self.abstract_class = true
 
-  connects_to database: { writing: :tenant_1, reading: :tenant_1 }
+  connects_to shards: {
+    shard_tenant_1: { writing: :tenant_1, reading: :tenant_1 },
+    shard_tenant_2: { writing: :tenant_2, reading: :tenant_2 },
+  }
 end
